21ce62e71f550b23f319933aa9b71d523dea1e7a
Richard <20459042+SignalRichard@users.noreply.github.com>
2023-04-21 11:08:59 -0400

    feat: init

---
+++ lib/Planet.rb
@@ -0,0 +1,3 @@
+class Planet
+    attr_accesor :name, :description, :aphelion, :perihelion, :eccentricity, :orbital_period, :average_orbital_speed, :satellites
+end
---
+++ lib/SolarSystem.rb
@@ -0,0 +1,13 @@
+module SolarSystem
+    Sun = 0
+    Mercury = 1
+    Venus = 2
+    Earth = 3
+    Ceres = 4
+    Mars = 5
+    Jupiter = 6
+    Saturn = 7
+    Uranus = 8
+    Neptune = 9
+    Pluto = 10
+end
